ede2009c7f22ee2d0ae5c03d8a538b5e30dc38b9
Syntaxical <193889244+xxpwnxxx420lord@users.noreply.github.com>
2025-07-06 15:40:42 +1000

    Create black.lua

---
+++ shh/black.lua
@@ -0,0 +1,12 @@
+--[[
+pray your not in here ;)
+
+this is the blacklist list please terroise everybody you see in here 
+(JOKE) please dont send death threats or terroize them please thats actually so mean
+]]
+
+local list = {
+    
+}
+
+return list
