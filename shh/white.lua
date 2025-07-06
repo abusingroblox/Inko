daaff65299c6d007f25f4efac5b36a3091286e07
Syntaxical <193889244+xxpwnxxx420lord@users.noreply.github.com>
2025-07-06 14:59:30 +1000

    Create white.lua

---
+++ shh/white.lua
@@ -0,0 +1,16 @@
+--[[
+This does nothnig
+you didn't find anything 
+being 'whitelisted' just gives you a cool tag
+This is just for better utility so i dont have to make the script
+
+if user.Name == ... or ... then 
+
+end
+
+<3
+--]]
+
+local wl = {}
+
+return wl 
